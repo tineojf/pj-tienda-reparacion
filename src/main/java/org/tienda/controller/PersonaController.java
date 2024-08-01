820e9901f326eb99c6a2200bb0a9a5a54dbbd036
tineojf <92743762+tineodev@users.noreply.github.com>
2024-08-01 12:21:09 -0500

    Add methods for persona controller

--- src/main/java/org/tienda/controller/PersonaController.java
+++ src/main/java/org/tienda/controller/PersonaController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.tienda.entity.PersonaModel;
+import org.tienda.exceptions.EntityAlreadyExistsException;
+import org.tienda.exceptions.EntityNotFoundException;
 import org.tienda.service.PersonaService;
 
 import java.util.List;
@@ -25,7 +27,6 @@ public class PersonaController {
                 cr = new CustomResponse(true, "Personas encontradas", personas);
             }
             return ResponseEntity.status(200).body(cr);
-
         } catch (Exception e) {
             CustomResponse cr = new CustomResponse(false, "ERROR DB", e.getMessage());
             return ResponseEntity.status(500).body(cr);
@@ -39,7 +40,9 @@ public class PersonaController {
             CustomResponse cr = new CustomResponse(true, "Persona encontrada", persona);
             return ResponseEntity.status(200).body(cr);
 
-            // add exception notfound
+        } catch (EntityNotFoundException e) {
+            CustomResponse cr = new CustomResponse(false, "Persona no encontrada", e.getMessage());
+            return ResponseEntity.status(404).body(cr);
         } catch (Exception e) {
             CustomResponse cr = new CustomResponse(false, "ERROR DB", e.getMessage());
             return ResponseEntity.status(500).body(cr);
@@ -53,10 +56,49 @@ public class PersonaController {
             CustomResponse cr = new CustomResponse(true, "Persona guardada", persona);
             return ResponseEntity.status(201).body(cr);
 
-            // add exception already exists
+        } catch (EntityAlreadyExistsException e) {
+            CustomResponse cr = new CustomResponse(false, "Persona ya existe", e.getMessage());
+            return ResponseEntity.status(400).body(cr);
         } catch (Exception e) {
             CustomResponse cr = new CustomResponse(false, "ERROR DB", e.getMessage());
             return ResponseEntity.status(500).body(cr);
         }
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomResponse> updatePersona(@PathVariable Long id, @RequestBody PersonaModel personaModel) {
+        try {
+            personaModel.setId(id);
+            PersonaModel persona = personaService.update(personaModel);
+            CustomResponse cr = new CustomResponse(true, "Persona actualizada", persona);
+            return ResponseEntity.status(200).body(cr);
+
+        } catch (EntityNotFoundException e) {
+            CustomResponse cr = new CustomResponse(false, "Persona no encontrada", e.getMessage());
+            return ResponseEntity.status(404).body(cr);
+        } catch (EntityAlreadyExistsException e) {
+            CustomResponse cr = new CustomResponse(false, "Persona ya existe", e.getMessage());
+            return ResponseEntity.status(400).body(cr);
+        } catch (Exception e) {
+            CustomResponse cr = new CustomResponse(false, "ERROR DB", e.getMessage());
+            return ResponseEntity.status(500).body(cr);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<CustomResponse> deletePersona(@PathVariable Long id) {
+        try {
+            PersonaModel persona = personaService.delete(id);
+            CustomResponse cr = new CustomResponse(true, "Persona eliminada", persona);
+            return ResponseEntity.status(200).body(cr);
+
+        } catch (EntityNotFoundException e) {
+            CustomResponse cr = new CustomResponse(false, "Persona no encontrada", e.getMessage());
+            return ResponseEntity.status(404).body(cr);
+        } catch (Exception e) {
+            CustomResponse cr = new CustomResponse(false, "ERROR DB", e.getMessage());
+            return ResponseEntity.status(500).body(cr);
+        }
+    }
+
 }
