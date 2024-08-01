86035a4a4d3ceb581ed55b9326838e1bd050d634
tineojf <92743762+tineodev@users.noreply.github.com>
2024-08-01 10:53:30 -0500

    Add methods for persona service

--- src/main/java/org/tienda/repository/PersonaRepository.java
+++ src/main/java/org/tienda/repository/PersonaRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.tienda.entity.PersonaModel;
 
+import java.util.Optional;
+
 @Repository
 public interface PersonaRepository extends JpaRepository<PersonaModel, Long> {
+
+    Optional<PersonaModel> findByNombre(String name);
+
+    Optional<PersonaModel> findByApellido(String apellido);
 }
--- src/main/java/org/tienda/service/PersonaService.java
+++ src/main/java/org/tienda/service/PersonaService.java
@@ -1,5 +1,70 @@
 package org.tienda.service;
 
-public class PersonaService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.tienda.entity.PersonaModel;
+import org.tienda.repository.PersonaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PersonaService implements IService<PersonaModel> {
+    @Autowired
+    private PersonaRepository personaRepository;
+
+    @Override
+    public List<PersonaModel> findAll() {
+        return personaRepository.findAll();
+    }
+
+    @Override
+    public PersonaModel findById(Long id) {
+        Optional<PersonaModel> personaModel = personaRepository.findById(id);
+
+        if (personaModel.isPresent()) {
+            return personaModel.get();
+        } else {
+            // notfound
+            return null;
+        }
+    }
+
+    @Override
+    public PersonaModel save(PersonaModel personaModel) {
+        Optional<PersonaModel> personaEqualsByNombre = personaRepository.findByNombre(personaModel.getNombre());
+        Optional<PersonaModel> personaEqualsByApellido = personaRepository.findByApellido(personaModel.getApellido());
+
+        if (personaEqualsByNombre.isPresent() || personaEqualsByApellido.isPresent()) {
+            // already exists
+            return null;
+        } else {
+            return personaRepository.save(personaModel);
+        }
+    }
+
+    @Override
+    public PersonaModel update(PersonaModel personaModel) {
+        Optional<PersonaModel> personaFindById = personaRepository.findById(personaModel.getId());
+
+        if (personaFindById.isPresent()) {
+            return personaRepository.save(personaModel);
+        } else {
+            // notfound
+            return null;
+        }
+    }
+
+    @Override
+    public PersonaModel delete(Long id) {
+        Optional<PersonaModel> personaFindById = personaRepository.findById(id);
+
+        if (personaFindById.isPresent()) {
+            personaRepository.deleteById(id);
+            return personaFindById.get();
+        } else {
+            // notfound
+            return null;
+        }
+    }
 }
